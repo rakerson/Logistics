3d82f5bd641803e444f7a86740377022fa533453
Robert Akerson <rakerson@hotmail.com>
2012-12-12 19:29:05 -0600

    --

--- Testing/Data/Levels/Level.h
+++ Testing/Data/Levels/Level.h
@@ -11,6 +11,7 @@
     int _number;
     BOOL _unlocked;
     int _stars;
+    int _userLastStars;
     NSString *_data;
     int _belts;
     int _fans;
@@ -18,6 +19,7 @@
     int _userLastScore;
     int _userHighScore;
     NSString *_background;
+    NSString *_item;
 }
 
 // Declare variable properties without an underscore
@@ -25,6 +27,7 @@
 @property (nonatomic, assign) int number;
 @property (nonatomic, assign) BOOL unlocked;
 @property (nonatomic, assign) int stars;
+@property (nonatomic, assign) int userLastStars;
 @property (nonatomic, copy) NSString *data;
 @property (nonatomic, assign) int belts;
 @property (nonatomic, assign) int fans;
@@ -32,19 +35,23 @@
 @property (nonatomic, assign) int userLastScore;
 @property (nonatomic, assign) int userHighScore;
 @property (nonatomic, copy) NSString *background;
+@property (nonatomic, copy) NSString *item;
+
 
 // Custom init method interface
 - (id)initWithName:(NSString *)name 
             number:(int)number 
           unlocked:(BOOL)unlocked 
-             stars:(int)stars 
+             stars:(int)stars
+     userLastStars:(int)userLastStars
               data:(NSString *)data
              belts:(int)belts
              fans:(int)fans
              springs:(int)springs
 userHighScore:(int)userHighScore
 userLastScore:(int)userLastScore
-background:(NSString *)background;
+background:(NSString *)background
+item:(NSString *)item;
 
 
 
