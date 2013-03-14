23afa97bbce75baba4af5ff170e4043b886b210c
Robert Akerson <rakerson@hotmail.com>
2013-03-13 22:16:56 -0500

    -

---
+++ build/Logistics.build/Debug-iphoneos/Logistics.build/Objects-normal/armv7/RootViewControllerInterface.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/rakerson/Desktop/Projects/Logistics/Testing/RootViewControllerInterface.m \
+  /Users/rakerson/Desktop/Projects/Logistics/Testing/RootViewControllerInterface.h \
+  /Users/rakerson/Desktop/Projects/Logistics/Testing/Data/GameData/GameData.h \
+  /Users/rakerson/Desktop/Projects/Logistics/Testing/Data/GameData/GameDataParser.h
